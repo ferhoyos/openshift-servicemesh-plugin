7f9046c13321a7f270bc8f09ec8a05945aaeddc4
John Mazzitelli <mazz@redhat.com>
2024-05-20 11:45:40 -0400

    release workflow action: change version strings properly

--- hack/update-version-string.sh
+++ hack/update-version-string.sh
@@ -11,15 +11,22 @@ NEXT_VERSION="${1:-}"
 [ -z "${NEXT_VERSION}" ] && (echo "Missing version string argument"; exit 1)
 [[ ${NEXT_VERSION} != v* ]] && (echo "The version string must start with 'v' [${NEXT_VERSION}]"; exit 1)
 
+# If NEXT_VERSION is a snapshot version, strip "-SNAPSHOT" and use that as the UI version.
+if [[ "${NEXT_VERSION}" == *-SNAPSHOT ]]; then
+  UI_NEXT_VERSION="${NEXT_VERSION%-SNAPSHOT}"
+else
+  UI_NEXT_VERSION="${NEXT_VERSION}"
+fi
+
 # Change to the directory to the root dir (which is the parent directory of this script)
 SCRIPT_ROOT="$( cd "$(dirname "$0")" ; pwd -P )"
 cd ${SCRIPT_ROOT}/..
 
 # Change the backend version
-sed -i -r "s/^VERSION \?= (.*)/VERSION \?= $NEXT_VERSION/" Makefile
+sed -i -r "s/^VERSION \?= (.*)/VERSION \?= ${NEXT_VERSION}/" Makefile
 
 # Change the UI version
-jq -r ".version |= \"${NEXT_VERSION:1}\"" plugin/package.json > plugin/package.json.tmp
+jq -r ".version |= \"${UI_NEXT_VERSION:1}\"" plugin/package.json > plugin/package.json.tmp
 mv plugin/package.json.tmp plugin/package.json
 
-sed -i -r "s/^  version: '(.*)',/  version: '${NEXT_VERSION:1}'/" plugin/plugin-metadata.ts
+sed -i -r "s/^  version: '(.*)',/  version: '${UI_NEXT_VERSION:1}',/" plugin/plugin-metadata.ts
